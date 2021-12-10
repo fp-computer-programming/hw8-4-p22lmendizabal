48f1f004657d255e8ad0757f8349a532cd98c0ca
p22mendizabal <p22lmendizabal@fairfieldprep.org>
2021-12-10 01:10:07 -0500

    add file

--- hw8-4-1.py
+++ hw8-4-1.py
@@ -1,22 +1,20 @@
 # author: LM (AMDG) 12/9/21
+
 import random
-random.seed(17)
-def guessing(number):
+def game():
     integer = random.randint(0, 50)
-    while number != integer:
-        if number < integer:
-            print("Guess a higher number")
-            input("Enter another number: ")
-        elif number > integer:
-            print("Guess a lower number.")
-            input("Enter another number.")
-        elif str(number) == integer:
-            print("The number was {0}".format(integer))
-            break
+    num = (input("Enter a number: "))
+    if (num) == "stop":
+        print("The number was {0}".format(integer))
+    while int(num) != integer:
+        if int(num)< integer:
+            print("Enter a higher number: ")
+           
         else:
-            return("You guessed the number. It was {0}".format(integer))
-            
-
-
-number = int(input("Enter a number: "))
-guessing(number)
+            print("Enter a lower number")
+        
+        
+    if int(num) == integer:
+        print("You guessed the number {0}.".format(integer))
+    return
+game()
